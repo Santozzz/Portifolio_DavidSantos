e9132accf3f85d74156d3267a973a7e44a57ce55
David Da Silva Santos <107252394+Santozzz@users.noreply.github.com>
2024-02-20 21:40:08 -0300

    Add files via upload

--- js/link-page.js
+++ js/link-page.js
@@ -1,19 +1,19 @@
 const about = document.querySelector('.about');
 
 about.addEventListener('click', () =>{
-    window.scroll({top: 600, behavior: "smooth"})
+    window.scroll({top: 580, behavior: "smooth"})
 })
 
 const button = document.querySelector('.button');
 
 button.addEventListener('click', () =>{
-    window.scroll({top: 600, behavior: "smooth"})
+    window.scroll({top: 580, behavior: "smooth"})
 })
 
 const portifolio = document.querySelector('.portifolio');
 
 portifolio.addEventListener('click', () =>{
-    window.scroll({top: 1050, behavior: "smooth"})
+    window.scroll({top: 1150, behavior: "smooth"})
 })
 
 const contato = document.querySelector('.contato');
